6f1c2b92f9582b895d24266b83bd2d39d9122bd3
ss <123@qq.com>
2021-01-22 09:26:07 +0800

    flask 的 helloworld

---
+++ project1/helloworld.py
@@ -0,0 +1,23 @@
+"""
+author:czy
+creat_date: 2021/1/22 9:20
+u_date: 2021/1/22 9:20
+reversion:1.0
+file_name: helloworld
+"""
+from flask import Flask
+
+helloworld=Flask(__name__)
+
+if __name__ == '__main__':
+    helloworld.run()
+
+
+
+
+
+
+
+
+
+
